ff5b22bc5618f25f465d20f581cd5e73f7827333
Paul Allies <paul@nanosoft.co.za>
2024-11-07 11:49:29 +0200

    Added crud forms

--- api/routes/users.py
+++ api/routes/users.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request, current_app, render_template
+from flask import Blueprint, jsonify, request, current_app, render_template, redirect
 from bson import ObjectId
 
 router = Blueprint('users', __name__, url_prefix="/users")
@@ -14,10 +14,17 @@ def get_users():
 @router.route("/", methods=["POST"])
 def insert_user():
     db = current_app.db
-    user = request.json
-    result = db.get_collection("users").insert_one(user)
-    # return jsonify({"id": str(result.inserted_id)}), 201
+    user_data = request.form.to_dict() 
+    result = db.get_collection("users").insert_one(user_data)
+    return redirect("/users")
 
+
+@router.route("/<id>", methods=["POST"])
+def form_update_user(id):
+    db = current_app.db
+    user = request.form.to_dict() 
+    db.get_collection("users").update_one({"_id": ObjectId(id)}, {"$set": user})
+    return redirect("/users", code=302)
 
 @router.route("/<id>", methods=["DELETE"])
 def delete_user(id):
@@ -27,9 +34,14 @@ def delete_user(id):
 
 @router.route("/<id>", methods=["GET"])
 def get_user(id):
+
+    if(id == "new"):
+        return render_template("user_new.html",)
+    
     db = current_app.db
     result = db.get_collection("users").find_one({"_id": ObjectId(id)})
     result["_id"] = str(result["_id"])
+    
     return render_template("user_detail.html", user=result)
     # return jsonify(result), 200
 
